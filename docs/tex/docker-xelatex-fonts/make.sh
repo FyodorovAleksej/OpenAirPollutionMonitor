1ea2a0dadcaa57d4a490500d7ad4795cdba73a3a
Alexey <Fyodorov.aleksej@gmail.com>
2019-03-29 01:45:51 -0400

    adding template building

--- docs/tex/docker-xelatex-fonts/make.sh
+++ docs/tex/docker-xelatex-fonts/make.sh
@@ -9,4 +9,6 @@ docker stop $NAME
 docker rm $NAME
 
 docker build -t $IMAGE .
-docker run -it -u root --name $NAME -v $HOST_FOLDER:$CONTAINER_FOLDER -w $CONTAINER_FOLDER $IMAGE
+docker run -u root --name $NAME -v $HOST_FOLDER:$CONTAINER_FOLDER -w $CONTAINER_FOLDER $IMAGE $@
+docker stop $NAME
+docker rm $NAME
